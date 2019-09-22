1285455c014d1675c3969bb1485a9ddbd4ed510f
Matthew Brun <meb21@rice.edu>
2019-09-22 07:18:45 -0500

    returns similar photo paths

--- usemodel.py
+++ usemodel.py
@@ -4,7 +4,7 @@ import os
 import numpy as np
 from matplotlib.image import imread
 import pickle
-
+import random
 
 
 def classify_photo(photo, gender):
@@ -22,4 +22,14 @@ def classify_photo(photo, gender):
 
 def similar_photos(group, gender):
     filedir = ["/Users/matthewbrun/Desktop/Tinder/Women", "/Users/matthewbrun/Desktop/Tinder/Men"]
+    fileclass = ["/Users/matthewbrun/Fall 2019/HackRice9/femaleclasslist.p", "/Users/matthewbrun/Fall 2019/HackRice9/maleclasslist.p"]
+    fileclasses = pickle.load(open(fileclass[gender],"rb"))
+    photos = fileclasses[group]
+    fileindex = random.sample(range(len(photos)),8)
+    files = []
+    for i in fileindex:
+        files.append(filedir[gender]+photos[i])
+    return files
 
+
+print(similar_photos(12,1))
